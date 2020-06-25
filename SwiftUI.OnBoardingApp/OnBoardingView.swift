41d73d249d73195545dfa2be1d960f5826a9ae69
Simran <simran5590@gmail.com>
2020-06-24 21:53:54 -0700

    changing state and titles

--- SwiftUI.OnBoardingApp/OnBoardingView.swift
+++ SwiftUI.OnBoardingApp/OnBoardingView.swift
@@ -16,8 +16,44 @@ struct OnBoardingView: View {
         UIHostingController(rootView: SubView(imageString: "sitting"))
     ]
     
+    var titles = ["Take some time out",
+                  "Conquer personal hindrances",
+                  "Create a peaceful mind"]
+    var captions = ["Take your time out and bring awareness into your everyday life",
+                    "Meditating helps you dealing with anxiety and other psychic problems",
+                    "Regular medidation sessions creates a peaceful inner mind"]
+    
+    @State var currentPageIndex = 0
+    
+    
     var body: some View {
-        PageViewController(viewController: subViews).frame(height: 600)
+        VStack {
+            PageViewController(viewController: subViews).frame(height: 600)
+            Button(action: {
+                if self.currentPageIndex+1 == self.subViews.count {
+                    self.currentPageIndex = 0
+                } else {
+                    self.currentPageIndex += 1
+                }
+            }) {
+                Image(systemName: "arrow.right")
+                    .resizable()
+                    .foregroundColor(.white)
+                    .frame(width: 30, height: 30)
+                    .padding()
+                    .background(Color.orange)
+                    .cornerRadius(30)
+            }
+            Text(titles[currentPageIndex])
+                .font(.title)
+            .frame(width: 300, alignment: .center)
+            Text(captions[currentPageIndex])
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .frame(width: 300, height: 50, alignment: .center)
+                .lineLimit(nil)
+            
+        }
     }
 }
 
